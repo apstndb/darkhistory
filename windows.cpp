b37217d19c49f286c271beb01f8bdee91f39e8a5
gama@hoge <gama@hoge>
2007-11-20 17:49:25 +0900

    複数ファイルのコミット忘れを修正

---
+++ windows.cpp
@@ -0,0 +1,10 @@
+#include <windows.h>
+#include "darkhistory.hpp"
+int WINAPI WinMain( HINSTANCE hInstance,
+		HINSTANCE hPrevInstance, 
+		LPSTR lpCmdLine,
+		int nCmdShow     
+		)
+{
+	return mymain();
+}
